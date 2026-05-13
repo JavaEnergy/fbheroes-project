bf32568dc04fe82ebc9e16e2bb71042ff7d6cfee
Beka Chogovadze <chogovadzebeka@gmail.com>
2026-05-13 15:37:25 +0400

    vend robotic from footer is deleted

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,8 +15,7 @@ export default function Footer(props: { dict: any; lang: string }) {
           <LinkItem href="/contact">
             F&B Heroes ({props.dict.navigation.contact})
           </LinkItem>
-          <LinkItem href="/robotic-gastronomy">VEND Robotic</LinkItem>
-          <LinkItem href="https://tamtam.hamburg/de" target="_blank">TAMTAM</LinkItem>
+<LinkItem href="https://tamtam.hamburg/de" target="_blank">TAMTAM</LinkItem>
         </Group>
         <Group>
           <GroupTitle style={{ color: "var(--primary-green)" }}>
